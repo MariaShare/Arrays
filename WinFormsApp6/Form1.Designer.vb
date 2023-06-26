ca5173dc0a5ddca4fe4ed82895f1dad694c6fac6
Mahadevii <mshadrina@gmail.com>
2023-06-26 06:23:20 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp6/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        ListBox2 = New ListBox()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(277, 12)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(163, 444)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(168, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.ItemHeight = 20
+        ListBox2.Location = New Point(446, 12)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(163, 444)
+        ListBox2.TabIndex = 2
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 14)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(67, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(616, 464)
+        Controls.Add(TextBox1)
+        Controls.Add(ListBox2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WinFormsApp6/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Text = "Массивы"
+    End Sub
+    Private Sub Massiv() Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Введите количество элементов массива!")
+        Else
+            Dim mass = Rand(TextBox1.Text)
+            For i = 0 To CInt(TextBox1.Text) - 1
+                ListBox1.Items.Add(mass(i))
+                If mass(i) Mod 2 Then
+                    mass(i) = -1
+                Else
+                    mass(i) = 1
+                End If
+                ListBox2.Items.Add(mass(i))
+            Next
+        End If
+
+
+    End Sub
+
+    Private Function Rand(ind As Integer) As Integer()
+        Dim mass(ind - 1) As Integer
+        For i = 0 To ind - 2
+            Randomize()
+            Dim randomValue = CInt(-200 * Rnd() + 100)
+            mass(i) = randomValue
+        Next
+        Return mass
+    End Function
+End Class
